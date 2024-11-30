224f4954e211d6862c32e1df82cdad57ced7b9c6
XLFrie <xlfrys@gmail.com>
2024-11-29 23:04:29 -0800

    Add unit test framework

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import ChatHelper, { Colors } from "utils/ChatHelper";
 import ControlBlockBreakProtection from "./events/ControlBlockBreakProtection";
 import ControlBlockPlaceProtection from "./events/ControlBlockPlaceProtection";
 import ParticleForcefield from "./intervals/ParticleForcefield";
+import { UnitTestregister } from "./tests/UnitTestRegister";
 
 world.afterEvents.worldInitialize.subscribe(() => {
 	ChatHelper.broadcastMessage(Colors.GREEN + Colors.BOLD + "Loaded");
@@ -15,3 +16,5 @@ EventManager.registerEvents([
 	new ControlBlockBreakProtection(),
 	new ControlBlockPlaceProtection(),
 ]);
+
+UnitTestregister.register();
---
+++ src/tests/UnitTestRegister.ts
@@ -0,0 +1,3 @@
+export class UnitTestregister {
+	public static register() {}
+}
