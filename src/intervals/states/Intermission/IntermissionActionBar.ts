61d7e6d37b387cfdb68dc59932b5b5a755c52502
XLFrie <xlfrys@gmail.com>
2024-11-30 14:13:55 -0800

    Fix actionbar

--- src/intervals/states/Intermission/IntermissionActionBar.ts
+++ src/intervals/states/Intermission/IntermissionActionBar.ts
@@ -1,4 +1,4 @@
-import { system, world } from "@minecraft/server";
+import { world } from "@minecraft/server";
 import { Colors } from "utils/ChatHelper";
 import { State } from "../../../GameStateManager";
 import StateInterval from "../StateInterval";
@@ -9,35 +9,33 @@ export default class ActionBar implements StateInterval {
 
 	public handlesState: State = State.INTERMISSION;
 	private wheel = ["\\^v^/", "/^v^\\"];
+	private iteration: number = 0;
 
 	public execute = async () => {
-		if (system.currentTick % this.delay === 0) {
-			let string = "INTERMISSION";
+		let string = "INTERMISSION";
 
-			const wheelIndex =
-				(system.currentTick / this.delay) % this.wheel.length;
-			const stringIndex =
-				(system.currentTick / this.delay) % (string.length + 8);
+		const wheelIndex = this.iteration % this.wheel.length;
+		const stringIndex = this.iteration % (string.length + 8);
 
-			if (stringIndex < string.length) {
-				let buff = [...string];
-				buff[stringIndex] = buff[stringIndex].toLowerCase();
+		if (stringIndex < string.length) {
+			let buff = [...string];
+			buff[stringIndex] = buff[stringIndex].toLowerCase();
 
-				string = buff.join("");
-			}
-
-			for (const player of world.getAllPlayers()) {
-				player.onScreenDisplay.setActionBar(
-					`${Colors.BOLD}${this.wheel[wheelIndex]} ${
-						Colors.GREEN
-					}${string}${Colors.RESET} ${Colors.BOLD}${
-						this.wheel[this.wheel.length - 1 - wheelIndex]
-					}`
-				);
-
-				player.onScreenDisplay.hideAllExcept;
-			}
+			string = buff.join("");
 		}
+
+		for (const player of world.getAllPlayers()) {
+			player.onScreenDisplay.setActionBar(
+				`${Colors.BOLD}${this.wheel[wheelIndex]} ${
+					Colors.GREEN
+				}${string}${Colors.RESET} ${Colors.BOLD}${
+					this.wheel[this.wheel.length - 1 - wheelIndex]
+				}`
+			);
+		}
+
+		this.iteration++;
+
 		return 0;
 	};
 }
