4af088a05aca05e48963fbe48e1b9ef2a407d9d8
XLFrie <xlfrys@gmail.com>
2024-11-30 07:28:22 -0800

    Fix capitalization

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import ControlBlockBreakProtection from "./events/ControlBlockBreakProtection";
 import ControlBlockPlaceProtection from "./events/ControlBlockPlaceProtection";
 import { GameStateManager } from "./GameStateManager";
 import ParticleForcefield from "./intervals/ParticleForcefield";
-import { UnitTestregister } from "./tests/UnitTestRegister";
+import { UnitTestRegister } from "./tests/UnitTestRegister";
 
 world.afterEvents.worldInitialize.subscribe(() => {
 	GameStateManager.loadGameState();
@@ -19,5 +19,5 @@ EventManager.registerEvents([
 	new ControlBlockPlaceProtection(),
 ]);
 
-UnitTestregister.register();
+UnitTestRegister.register();
 ChatHelper.setLogLevel(LOG_LEVEL.VERBOSE);
