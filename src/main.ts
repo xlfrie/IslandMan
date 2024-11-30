c4f2a9272fd005d84d94c321756e41f160a4855b
XLFrie <xlfrys@gmail.com>
2024-11-29 23:46:29 -0800

    Add chat logging

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { world } from "@minecraft/server";
 import EventManager from "managers/EventManager";
 import SystemIntervalManager from "managers/SystemIntervalManager";
-import ChatHelper, { Colors } from "utils/ChatHelper";
+import ChatHelper, { Colors, LOG_LEVEL } from "utils/ChatHelper";
 import ControlBlockBreakProtection from "./events/ControlBlockBreakProtection";
 import ControlBlockPlaceProtection from "./events/ControlBlockPlaceProtection";
+import { GameStateManager } from "./GameStateManager";
 import ParticleForcefield from "./intervals/ParticleForcefield";
 import { UnitTestregister } from "./tests/UnitTestRegister";
 
 world.afterEvents.worldInitialize.subscribe(() => {
-	ChatHelper.broadcastMessage(Colors.GREEN + Colors.BOLD + "Loaded");
+	GameStateManager.loadGameState();
+	ChatHelper.log(Colors.GREEN + Colors.BOLD + "Loaded", LOG_LEVEL.VERBOSE);
 });
 
 SystemIntervalManager.registerIntervals([new ParticleForcefield()]);
@@ -18,3 +20,4 @@ EventManager.registerEvents([
 ]);
 
 UnitTestregister.register();
+ChatHelper.setLogLevel(LOG_LEVEL.VERBOSE);
