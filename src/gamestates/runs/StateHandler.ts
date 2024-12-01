d6f3842bdbea9a5384cbe2a8f7f690ea2664fa50
XLFrie <xlfrys@gmail.com>
2024-11-30 19:56:55 -0800

    Implement player init

--- src/gamestates/runs/StateHandler.ts
+++ src/gamestates/runs/StateHandler.ts
@@ -1,4 +1,9 @@
-import { Player, system } from "@minecraft/server";
+import {
+	Player,
+	PlayerSpawnAfterEvent,
+	system,
+	world,
+} from "@minecraft/server";
 import EventManager from "managers/EventManager";
 import SystemIntervalManager from "managers/SystemIntervalManager";
 import Event from "types/Event";
@@ -15,16 +20,46 @@ export default abstract class StateHandler {
 
 	public start() {
 		this.runIDs = this.runIDs.concat(
-			SystemIntervalManager.registerIntervals(this.intervals),
-			EventManager.registerEvents(this.events)
+			SystemIntervalManager.registerIntervals(this.intervals)
 		);
+
+		EventManager.registerEvents(this.events);
+
+		world.afterEvents.playerSpawn.subscribe(this.playerInit);
+
+		for (const player of world.getAllPlayers()) {
+			this.playerInit(player);
+		}
 	}
 
 	public end() {
 		for (const id of this.runIDs) {
 			system.clearRun(id);
 		}
+
+		for (const event of this.events) {
+			event.eventSignal.unsubscribe(event.execute);
+		}
+
+		world.afterEvents.playerSpawn.unsubscribe(this.playerJoin);
 	}
 
-	public abstract playerInit(player: Player): void;
+	public playerJoin(event: PlayerSpawnAfterEvent) {
+		console.log(event.player.name);
+		if (event.initialSpawn) this.playerInit(event.player);
+	}
+
+	playerInit(event: Player | PlayerSpawnAfterEvent): Player | void;
+
+	public playerInit(
+		playerOrEvent: Player | PlayerSpawnAfterEvent
+	): Player | void {
+		if ("initialSpawn" in playerOrEvent) {
+			if (playerOrEvent.initialSpawn) {
+				return playerOrEvent.player;
+			}
+		} else {
+			return playerOrEvent;
+		}
+	}
 }
--- src/gamestates/runs/intermission/Intermission.ts
+++ src/gamestates/runs/intermission/Intermission.ts
@@ -1,17 +1,37 @@
-import { Player } from "@minecraft/server";
+import { Player, PlayerSpawnAfterEvent, world } from "@minecraft/server";
 import Event from "types/Event";
 import Interval from "types/Interval";
+import { Vector } from "wrappers/Vector";
 import { State } from "../../GameStateManager";
 import StateHandler from "../StateHandler";
 import IntermissionActionBar from "./intervals/IntermissionActionBar";
 
 export default class Intermission extends StateHandler {
-	public override state: State = State.INTERMISSION;
-	public override events: Event[] = [];
+	public state: State = State.INTERMISSION;
+	public events: Event[] = [];
 
-	public override intervals: Interval[] = [new IntermissionActionBar()];
+	public intervals: Interval[] = [new IntermissionActionBar()];
 
-	public override playerInit(player: Player): void {
-		throw new Error("Method not implemented.");
+	public override playerInit(
+		playerOrEvent: Player | PlayerSpawnAfterEvent
+	): Player | void {
+		const player = super.playerInit(playerOrEvent);
+		if (!player) return;
+
+		// TODO save locations and restore if game has already started
+		if (!player.hasTag("dev")) {
+			player.onScreenDisplay.hideAllExcept();
+			player.teleport(new Vector(0.5, -63, 0.5));
+			player.addEffect("blindness", 20000000, { showParticles: false });
+		}
+	}
+
+	public override end() {
+		for (const player of world.getAllPlayers()) {
+			player.removeEffect("blindness");
+			player.onScreenDisplay.resetHudElements();
+		}
+
+		super.end();
 	}
 }
